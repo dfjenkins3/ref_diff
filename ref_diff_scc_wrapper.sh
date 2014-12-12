1f40341d87c78d0bd9fad22332db41c3007c4b39
David Jenkins <d.jenkins0@gmail.com>
2014-12-11 20:24:56 -0500

    Modified qsub script with changes needed for scc4

--- ref_diff_scc_wrapper.sh
+++ ref_diff_scc_wrapper.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 #. /etc/bashrc
 
@@ -18,7 +18,7 @@ module load tophat
 module load python2.7/Python-2.7.3_gnu446
 module load htseq/0.6.1p1
 
-~/ref_diff/ref_diff.py \
+python2.7 ~/ref_diff/ref_diff.py \
     --oldbam   /restricted/projectnb/mlpd/mRNA/alignment/hg38/C_0002/subset/hg19/accpeted_hits_chr21.hg19.bam \
     --oldun    /restricted/projectnb/mlpd/mRNA/alignment/hg19/C_0002/unmapped.bam \
     --fq1      /restricted/projectnb/mlpd/mRNA/alignment/hg38/C_0002/subset/C_0002_chr21_1.fastq.gz \
